cad347f42074c25577a0a5b584de43e42685f437
Ed <166579149+Edcookie@users.noreply.github.com>
2024-04-10 00:58:57 -0300

    if & else.c

--- if & else.c
+++ if & else.c
@@ -1,60 +1,64 @@
 Estudo básico (if e else)
 
+
 if e else:
-if: É uma estrutura condicional que permite executar um bloco de código se uma condição específica for verdadeira.
+
+  if: É uma estrutura condicional que permite executar um bloco de código se uma condição específica for verdadeira.
+
 
 Exemplo:
-
 
 
 if (condição) {
-    // Código a ser executado se a condição for verdadeira
+    Código a ser executado se a condição for verdadeira
 }
 
 
-else: É uma extensão do if que permite especificar um bloco de código a ser executado se a condição no if for falsa.
+  else: É uma extensão do if que permite especificar um bloco de código a ser executado se a condição no if for falsa.
 
 
 Exemplo:
 
 
-
-
- if (condição) {
-    // Código a ser executado se a condição for verdadeira
+if (condição) {
+    
+    Código a ser executado se a condição for verdadeira
+    
 } else {
-    // Código a ser executado se a condição for falsa
- }
-
+    
+    Código a ser executado se a condição for falsa
+}
 
 
 else if: É usado para adicionar outra condição à verificação, útil quando há mais de duas possibilidades de resultados.
 
+
 Exemplo:
 
 
-
-
- if (condição1) {
-    // Código a ser executado se a condição1 for verdadeira
+if (condição1) {
+    
+    Código a ser executado se a condição1 for verdadeira
+    
 } else if (condição2) {
-    // Código a ser executado se a condição1 for falsa e a condição2 for verdadeira
+    
+    Código a ser executado se a condição1 for falsa e a condição2 for verdadeira
+        
 } else {
-    // Código a ser executado se todas as condições anteriores forem falsas
+    
+    Código a ser executado se todas as condições anteriores forem falsas
+        
  }
-
 
 
 Exemplo de Uso:
 
 
-
-
-
- int main() {
+int main() {
     int numero = 10;
 
-    // Verificar se o número é positivo, negativo ou zero
+      Verifica se o número é positivo, negativo ou zero
+    
     if (numero > 0) {
         printf("O número é positivo.\n");
     } else if (numero < 0) {
@@ -64,22 +68,21 @@ Exemplo de Uso:
     }
 
     return 0;
- }
+}
 
 
 
 
-Neste exemplo, o programa verifica se o número é positivo, negativo ou zero. Se o número for maior que zero, ele imprime "O número é positivo.". Se for menor que zero, imprime "O número é negativo.". Se nenhuma dessas condições for verdadeira (ou seja, o número for igual a zero), ele imprime "O número é zero.".
+  Neste exemplo, o programa verifica se o número é positivo, negativo ou zero. Se o número for maior que zero, ele imprime "O número é positivo.". Se for menor que zero, imprime "O número é negativo.". Se nenhuma dessas condições for verdadeira (ou seja, o número for igual a zero), ele imprime "O número é zero.".
 
 
-Vou mostrar exemplos de como usar if e else com float, char e int.
+  Vou mostrar exemplos de como usar if e else com float, char e int.
 
   Exemplo com int:
 
 
-  
 
- int main() {
+int main() {
       int idade;
 
       printf("Digite sua idade: ");
